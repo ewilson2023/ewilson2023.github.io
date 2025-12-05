1f1d9255a83300d954126ae1fe64d1da5e2120bb
ewilson2023 <ewilson23@ubishops.ca>
2025-12-04 19:53:56 -0500

    removed need for p_container now that i know what a selector is

--- my_site/index.php
+++ my_site/index.php
@@ -24,27 +24,21 @@
 		  <div class="main">	
 		  
 		  	<div class="h2_decorated">
-		  		<hr>
-		  			<h2>Past</h2>
-		  		<hr>
+		  		<hr><h2>Past</h2><hr>
 		  	</div>
-		  		<h3 class="after_h2">Early Life</h3>
-		  			<div class="p_container">
-		  				<p> 
+		  		<h3>Early Life</h3>
+		  			<p> 
 		  				I was born in Ottawa. At home, I live with my parents, my
 		  				younger brother and sister, and my two dogs.
-		  				</p>
-		  			</div>
+		  			</p>
 		  		<h3>Early Education</h3>
-		  			<div class="p_container">
-		  				<p> 
+					<p> 
 		  				After high school I studied history at the University
 		  				of Ottawa for two years. Eventually, I realised I hated writing
 		  				essays and switched to Algonquin College for 
 		  				Concept Art &amp Illustration, a three semester program. I was
 		  				near the top of the class.
-		  				</p>
-		  			</div>
+		  			</p>
 		  			
 		  		<!-- IMAGE SLIDESHOW	-->			
 		  		<div class="slideshow">
@@ -68,37 +62,30 @@
 		  		</div>
 		  		
 		  		<h3>Work</h3>
-		  			<div class="p_container">
-		  				<p>
+					<p>
 		  				I made friends and connections at Algonquin that led
 		  				to work in the art industry. Most of my work was low-paying
 		  				commissions until 2019, when I was hired by Kindly Beast,
 		  				a game development studio based in Ottawa. It paid well and
 		  				I loved working there, but it closed less than a year later.
-		  				</p><p>
+		  			</p><p>
 		  				I would link my portfolio but it's not public right now because
 		  				I stopped giving Adobe any of my money.
-		  				</p>
-		  			</div>
+		  			</p>
 		  		<h3>Covid-19</h3>
-		  			<div class="p_container">
-		  				<p>
+					<p>
 		  				Covid hit a few months later. I was commissed by Apple UK
 		  				to make art for the app store's "Today Tab", but otherwise,
 		  				I didn't do much of anything.
-		  				</p><p>
+		  			</p><p>
 		  				In summer 2022, I used the money from Kindly Beast to go on a solo
 		  				trip to Ireland, where my grandparents were born. I was there for almost two months.
-		  				</p>
-		  			</div>
+		  			</p>
 		  			
 		  	<div class="h2_decorated">
-		  		<hr>
-		  			<h2>Present</h2>
-		  		<hr>
+				<hr><h2>Present</h2><hr>
 		  	</div>
-		  		<div class="p_container">
-		  			<p>
+		  		<p>
 		  			I loved Ireland so much I wanted to try living there, but I
 		  			had no reliable way to 
 		  			support myself.	I was already burnt out from the art industry 
@@ -106,8 +93,7 @@
 		  			to study computer science, something I had limited exposure to,
 		  			but felt I had an apitutude for. I started attending Bishop's
 		  			in 2023.
-		  			</p>
-		  		</div>
+		  		</p>
 		  </div>
 		</div>
 		
